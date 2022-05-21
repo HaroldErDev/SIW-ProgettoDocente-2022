4124e52d8a0258a45106136eae33c2cd14d6f2aa
HaroldDev <tinaritiziano.dev@gmail.com>
2022-05-21 13:07:12 +0200

    Half completed chef.html page

--- siw-spring-catering/src/main/java/com/example/demo/controller/ChefController.java
+++ siw-spring-catering/src/main/java/com/example/demo/controller/ChefController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.model.Chef;
 import com.example.demo.service.ChefService;
 
 @Controller
@@ -10,4 +15,12 @@ public class ChefController {
 	
 	@Autowired
 	private ChefService chefService;
+	
+	@GetMapping("/chef")
+	public String getAllChef(Model model) {
+		List<Chef> chef = chefService.findAll();
+		model.addAttribute("chef", chef);
+		return "chef.html";
+	}
+	
 }
--- siw-spring-catering/src/main/java/com/example/demo/service/ChefService.java
+++ siw-spring-catering/src/main/java/com/example/demo/service/ChefService.java
@@ -1,8 +1,12 @@
 package com.example.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.model.Chef;
 import com.example.demo.repository.ChefRepository;
 
 @Service
@@ -10,4 +14,15 @@ public class ChefService {
 	
 	@Autowired
 	private ChefRepository chefRepository;
+	
+	public List<Chef> findAll() {
+		List<Chef> chef = new ArrayList<>();
+		
+		for(Chef c: chefRepository.findAll()) {
+			chef.add(c);
+		}
+		
+		return chef;
+	}
+	
 }
