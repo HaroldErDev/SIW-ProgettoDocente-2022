91dbf926239f13d273a2645b0b2352e93f0ed068
HaroldDev <tinaritiziano.dev@gmail.com>
2022-05-23 19:05:46 +0200

    Added ingrediente.html and some refactoring

--- siw-spring-catering/src/main/java/com/example/demo/controller/IngredienteController.java
+++ siw-spring-catering/src/main/java/com/example/demo/controller/IngredienteController.java
@@ -2,7 +2,11 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.example.demo.model.Ingrediente;
 import com.example.demo.service.IngredienteService;
 
 @Controller
@@ -10,4 +14,12 @@ public class IngredienteController {
 	
 	@Autowired
 	private IngredienteService ingredienteService;
+	
+	@GetMapping("/ingrediente/{id}")
+	public String getIngrediente(@PathVariable("id") Long id, Model model) {
+		Ingrediente ingrediente = ingredienteService.findIngredienteById(id);
+		model.addAttribute("ingrediente", ingrediente);
+		return "ingrediente.html";
+	}
+	
 }
--- siw-spring-catering/src/main/java/com/example/demo/service/IngredienteService.java
+++ siw-spring-catering/src/main/java/com/example/demo/service/IngredienteService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.model.Ingrediente;
 import com.example.demo.repository.IngredienteRepository;
 
 @Service
@@ -10,4 +11,9 @@ public class IngredienteService {
 	
 	@Autowired
 	private IngredienteRepository ingredienteRepository;
+	
+	public Ingrediente findIngredienteById(Long id) {
+		return ingredienteRepository.findById(id).get();
+	}
+	
 }
